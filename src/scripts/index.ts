fb41a38f270a1ea6060b964386e1ee66ff1a04ea
David Condit <david.condit@InfoSync.Local>
2018-09-21 16:55:46 -0500

    Made sure typescript is working.

---
+++ src/scripts/index.ts
@@ -0,0 +1,4 @@
+import Logger from './logger';
+
+let logger = new Logger();
+logger.sayHello("David Condit");
---
+++ src/scripts/logger.ts
@@ -0,0 +1,8 @@
+export default class Logger {
+    constructor() {
+
+    }
+    sayHello(name: string): void {
+        console.log(`Hello, ${name}!`)
+    }
+}
